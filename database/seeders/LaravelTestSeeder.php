a738eda811bd6438254b2c7be6622246670acf5f
Oussama0529 <oussamanajib11@gmail.com>
2022-08-03 17:39:23 +0200

    second commit

--- database/seeders/LaravelTestSeeder.php
+++ database/seeders/LaravelTestSeeder.php
@@ -2,6 +2,7 @@
 namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\LaravelTest;
+use Illuminate\Support\Facades\DB;
 class LaravelTestSeeder extends Seeder
 {
     /**
@@ -14,8 +15,47 @@ class LaravelTestSeeder extends Seeder
         LaravelTest::truncate(); 
         $csvData = fopen(storage_path('\listeMat.csv'), 'r');
         $firstline = true; 
-        while (($data = fgetcsv($csvData,5000, ',')) !== false) {
+        $c=0;
+        while (($data = fgetcsv($csvData,5000, "\n")) !== false) {
             if (!$firstline) {
+                //
+                $colones = explode(';',($data[0])); 
+                //
+                print_r($colones);
+
+                $mv=floatval($colones[2]);
+                $long=floatval($colones[7]);
+                $larg=floatval($colones[8]);
+                $epai=floatval($colones[9]);
+                $vol=floatval($colones[10]);
+                ///
+                /*
+                print_r($mv."\n");
+                print_r($long."\n");
+                print_r($larg."\n");
+                print_r($epai."\n");
+                print_r($vol."\n");
+                */
+                //
+                $cd_str=str_replace(' ','',$colones[11]);
+                $cd=intval($cd_str);
+                
+                DB::table('laraveltest')->insert( [
+                    //'ref' => $data[0],
+                    'detail_dechet' => $colones[1],
+                    'masse_volumique' => $mv,
+                    'cerfa' =>$colones[3],
+                    'niv1' => $colones[4],
+                    'niv2' => $colones[5],
+                    'unité' => $colones[6],
+                    'longeur' => $long,
+                    'largeur' => $larg,
+                    'epaisseur' => $epai,
+                    'volume' => $vol,
+                    'code_dechet' => $cd,
+                    'lot' => $colones[12]
+                ]);
+                /*
                 LaravelTest::create([
                     'ref' => $data[0],
                     'detail_dechet' => $data[1],
@@ -31,6 +71,22 @@ class LaravelTestSeeder extends Seeder
                     'code_dechet' => $data[11],
                     'lot' => $data[12],
                 ]);
+                */
+                /*
+                $c++;
+                if($c==5){
+                    break;
+                }*/
+                //print_r($data) ;
+                //print_r($colones);
+                //print_r($cd."\n");
+                
+                
+
+
+
+
+
             }
             $firstline = false;
         }
