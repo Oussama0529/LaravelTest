68967826069db10895a8d720064072562abf1e5a
Oussama0529 <oussamanajib11@gmail.com>
2022-08-04 10:53:33 +0200

    This commit : add new csv file to storage & modified seeder and migrations files

--- database/migrations/2022_08_01_101018_create_laravel_tests_table.php
+++ database/migrations/2022_08_01_101018_create_laravel_tests_table.php
@@ -13,19 +13,19 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('LaravelTest', function (Blueprint $table) {
+        Schema::create('laraveltest', function (Blueprint $table) {
                 $table->increments('ref');
                 $table->string('detail_dechet');
-                $table->float('masse_volumique',8,2);
+                $table->double('masse_volumique',15,8);
                 $table->string('cerfa');
                 $table->string('niv1');
                 $table->string('niv2');
                 $table->string('unité');
-                $table->float('longeur',8,2);
-                $table->float('largeur',8,2);
-                $table->float('epaisseur',8,2);
-                $table->float('volume',8,2);
-                $table->integer('code_dechet');
+                $table->double('longeur',15,8);
+                $table->double('largeur',15,8);
+                $table->double('epaisseur',15,8);
+                $table->double('volume',15,8);
+                $table->string('code_dechet');
                 $table->string('lot');
                 $table->timestamps();
         });
@@ -38,6 +38,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('laravelTest');
+        Schema::dropIfExists('laraveltest');
     }
 };
--- database/seeders/LaravelTestSeeder.php
+++ database/seeders/LaravelTestSeeder.php
@@ -13,7 +13,7 @@ class LaravelTestSeeder extends Seeder
     public function run()
     {
         LaravelTest::truncate(); 
-        $csvData = fopen(storage_path('\listeMat.csv'), 'r');
+        $csvData = fopen(storage_path('\BDD_03082022.csv'), 'r');
         $firstline = true; 
         $c=0;
         while (($data = fgetcsv($csvData,5000, "\n")) !== false) {
@@ -23,22 +23,14 @@ class LaravelTestSeeder extends Seeder
                 //
                 print_r($colones);
 
-                $mv=floatval($colones[2]);
-                $long=floatval($colones[7]);
-                $larg=floatval($colones[8]);
-                $epai=floatval($colones[9]);
-                $vol=floatval($colones[10]);
-                ///
-                /*
-                print_r($mv."\n");
-                print_r($long."\n");
-                print_r($larg."\n");
-                print_r($epai."\n");
-                print_r($vol."\n");
-                */
-                //
-                $cd_str=str_replace(' ','',$colones[11]);
-                $cd=intval($cd_str);
+                $mv=doubleval(str_replace(',','.',$colones[2]));
+                $long=doubleval(str_replace(',','.',$colones[7]));
+                $larg=doubleval(str_replace(',','.',$colones[8]));
+                $epai=doubleval(str_replace(',','.',$colones[9]));
+                $vol=doubleval(str_replace(',','.',$colones[10]));
+                
+                //$cd_str=str_replace(' ','',$colones[11]);
+                //$cd=intval($cd_str);
                 
                 DB::table('laraveltest')->insert( [
                     //'ref' => $data[0],
@@ -52,40 +44,15 @@ class LaravelTestSeeder extends Seeder
                     'largeur' => $larg,
                     'epaisseur' => $epai,
                     'volume' => $vol,
-                    'code_dechet' => $cd,
+                    'code_dechet' => $colones[11],
                     'lot' => $colones[12]
                 ]);
-                /*
-                LaravelTest::create([
-                    'ref' => $data[0],
-                    'detail_dechet' => $data[1],
-                    'masse_volumique' => $data[2],
-                    'cerfa' => $data[3],
-                    'niv1' => $data[4],
-                    'niv2' => $data[5],
-                    'unité' => $data[6],
-                    'longeur' => $data[7],
-                    'largeur' => $data[8],
-                    'epaisseur' => $data[9],
-                    'volume' => $data[10],
-                    'code_dechet' => $data[11],
-                    'lot' => $data[12],
-                ]);
-                */
+                
                 /*
                 $c++;
                 if($c==5){
                     break;
                 }*/
-                //print_r($data) ;
-                //print_r($colones);
-                //print_r($cd."\n");
-                
-                
-
-
-
-
 
             }
             $firstline = false;
